4b7a592ae9791a1026fa215d93d7dca1160e22f1
K-Dongil <kdi1569@gmail.com>
2021-07-22 18:07:48 +0900

    파이썬 정리추가

---
+++ baekjoon/2869.py
@@ -0,0 +1,21 @@
+import sys
+A, B, V = map(int, sys.stdin.readline().split())
+def snail(A, B, V):
+    V - A*x +B*x < = A
+    return day
+print(snail(A, B, V))
+
+
+#=================================================================
+# import sys
+# afternoon, night, height = map(int, sys.stdin.readline().split())
+# def snail(afternoon, night, height):
+#     day = 0
+#     while True:
+#         height -= afternoon
+#         day +=1
+#         if height <=0:
+#             break
+#         height += night
+#     return day
+# print(snail(afternoon, night, height))
---
+++ baekjoon/4673.py
@@ -0,0 +1,6 @@
+def selfNumber(num):
+    
+    print(num)
+
+for i in range(1, 101):
+    selfNumber(i)
