a695deeff57fe117331cbcd77b80120ea9d561dc
K-Dongil <kdi1569@gmail.com>
2021-07-26 21:52:13 +0900

    달팽이문제ㅠㅠ 다시보자

--- baekjoon/2869(다시보자).py
+++ baekjoon/2869(다시보자).py
@@ -1,10 +1,24 @@
 import sys
 A, B, V = map(int, sys.stdin.readline().split())
-def snail(A, B, V):
-    day = V//(A-B)+1 if A-B !=1 else V//(A-B)-A+1
-    return day
-print(snail(A, B, V))
-#V - A*x +B*x <= A
+if (V-B)%(A-B) == 0:
+    print((V-B)//(A-B))
+else:
+    print((V-B)//(A-B)+1)
+
+#=====================================================
+# import sys
+# A, B, V = map(int, sys.stdin.readline().split())
+# def snail(A, B, V):
+#     result_day = 0
+#     if (V-B)%(A-B) == 0:
+#         result_day = (V-B)//(A-B)
+#     else:
+#         result_day = (V-B)//(A-B)+1
+#     return result_day
+# print(snail(A, B, V))
+
+
+#V - A*x +B*(x-1) <= 0 ----------> (V-B)/(A-B) <=x
 #=================================================================
 # import sys
 # afternoon, night, height = map(int, sys.stdin.readline().split())
