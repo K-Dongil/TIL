d2823adf747671beb351fafb4135778ae96d8215
K-Dongil <kdi1569@gmail.com>
2021-07-28 21:00:52 +0900

    오늘의 study

--- baekjoon/2753.py
+++ baekjoon/2753.py
@@ -1,5 +1,5 @@
-year = int(input())
-if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+year = int(input()) # 입력값을 받는다
+if (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)): # 4의 배수이면 True(다음 값과 비교, 100의 배수가 아닐 때 True or 400의 배수일 때 True) 아니면 False(조건식 False), 
     print('1')
 else:
     print('0') 
