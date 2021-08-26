e1c8d597f1017e6bcc3fd288f9f0f1e6348c7a4e
K-Dongil <kdi1569@gmail.com>
2021-08-26 17:59:49 +0900

    원형 큐를 구현해 문제풀이

---
+++ algorithm/ssafy/5097.py
@@ -0,0 +1,59 @@
+# tc = int(input())
+#
+# for t in range(tc):
+#     N, M = map(int, input().split())
+#     lst = list(map(int, input().split()))
+#
+#     for i in range(M):
+#         a = lst.pop(0)
+#         lst.append(a)
+#
+#     print('#{} {}'.format(t+1, lst[0]))
+
+
+
+# ======================================================================
+# 원형큐 구현
+def isEmpty():
+	return front == rear
+
+def isFull():
+	return (rear+1) % len(q) == front
+
+def enQueue(item):
+	global rear
+	if isFull():
+		print("Queue_Full")
+	else:
+		rear = (rear + 1) % len(q)
+		q[rear] = item
+
+def deQueue():
+	global front
+	if isEmpty():
+		print("Queue_Empty")
+	else:
+		front = (front + 1) % len(q)
+		return q[front]
+
+tc = int(input())
+
+for t in range(tc):
+    N, M = map(int, input().split())
+    lst = list(map(int, input().split()))
+    q = [0] * (N+1)
+
+    front = 0
+    rear = 0
+
+    for i in range(len(lst)): # 큐에 데이터가 없기 때문에, enQueue를 활용해 데이터를 넣어준다
+        enQueue(lst[i])
+
+    for i in range(M):
+        a = deQueue()
+        enQueue(a)
+        print(q)
+
+    # front는 마지막에 집어 넣은 값을 뜻하므로 front+1이여야한다
+    print('#{} {}'.format(t+1, q[(front + 1) % len(q)]))
+
