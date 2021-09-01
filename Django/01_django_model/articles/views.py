b5bfc65d5daedaad3d45045d50adb7dcdcda8566
K-Dongil <kdi1569@gmail.com>
2021-09-01 22:36:37 +0900

    CRUD 실습

--- Django/01_django_model/articles/views.py
+++ Django/01_django_model/articles/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
-    pass
-
-
+    # 작성한 모든 게시글을 출력
+    # 1. 모든 게시글 조회
+    articles = Article.objects.all()
+    print(articles)
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
 def new(request):
-    pass
+    return render(request, 'articles/new.html')
 
 
 def create(request):
-    pass
+    # new로부터 title과 content를 받아서 저장
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # 1
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+    # 2
+    article = Article(title = title, content = content)
+    article.save()
+    # 3
+    # Article.objects.create(title = title, content = content)
+    return render(request, 'articles/create.html')
 
 
 def detail(request, pk):
--- Django/01_django_model/crud/settings.py
+++ Django/01_django_model/crud/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'crud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR, BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
