79920e7c07835451e05d30362367b291d2431b1f
K-Dongil <kdi1569@gmail.com>
2021-09-01 12:00:28 +0900

    django model 실습

---
+++ Django/01_django_model/articles/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    pass
+
+
+def new(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def detail(request, pk):
+    pass
+
+
+def delete(request, pk):
+    pass
+
+
+def edit(request, pk):
+    pass
+
+
+def update(request, pk):
+    pass
