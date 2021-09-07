30c29e9401b9049b0b993cee770589ea8edc008b
K-Dongil <kdi1569@gmail.com>
2021-09-08 00:21:37 +0900

    1차원 배열, index 문제

--- swea/6730.py
+++ swea/6730.py
@@ -4,15 +4,15 @@ for t in range(tc):
     square = int(input())
     height = list(map(int, input().split()))
     max_up = 0
-    min_down = 0
+    max_down = 0
 
     for i in range(square-1):
         if height[i+1] > height[i]:
             up_dif = height[i+1] - height[i]
             if max_up < up_dif:
                 max_up = up_dif
-        if i < square-2 and height[i+2] < height[i+1]:
-            down_dif = height[i+1] - height[i+2]
-            if min_down > down_dif:
-                min_down = down_dif
-    print('#{} {} {}'.format(t+1,max_up, min_down))
+        if height[i+1] < height[i]:
+            down_dif = height[i] - height[i+1]
+            if max_down < down_dif:
+                max_down = down_dif
+    print('#{} {} {}'.format(t+1,max_up, max_down))
