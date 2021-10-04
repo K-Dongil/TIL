488aa9ac1f339714cf3cb88d3777d96ccd30dd33
K-Dongil <kdi1569@gmail.com>
2021-10-04 09:28:28 +0900

    16진수 2진수로 표현

---
+++ swea/5185.py
@@ -0,0 +1,21 @@
+def hexaToBinary(hexNum):
+    return dict[hexNum]
+
+dict = {
+        '0':'0000', '1':'0001', '2':'0010',
+        '3':'0011', '4':'0100', '5':'0101',
+        '6':'0110', '7':'0111', '8':'1000',
+        '9':'1001', 'A':'1010', 'B':'1011',
+        'C':'1100', 'D':'1101', 'E':'1110', 'F':'1111'
+    }
+
+tc = int(input())
+
+for t in range(1, tc+1):
+    N, hexas = input().split()
+    binarygin = ''
+
+    for hexa in hexas:
+        binarygin += hexaToBinary(hexa)
+    
+    print(f'#{t} {binarygin}')
