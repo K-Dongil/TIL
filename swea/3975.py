afc0b9c2b379530c55612d213db4ee734948ee28
K-Dongil <kdi1569@gmail.com>
2021-10-21 18:27:16 +0900

    시간초과로 인한 모든 테스트케이스의 결과를 저장시켜놓고 한번에 출력하기

---
+++ swea/3975.py
@@ -0,0 +1,14 @@
+tc = int(input())
+
+result = [0] * tc
+
+for t in range(0, tc):
+    elice_win, elice_play, bob_win, bob_play = map(int, input().split())
+    result[t] = "#" + str(t+1) + " DRAW"
+    if elice_win / elice_play > bob_win / bob_play:
+        result[t] = "#" + str(t+1) + " ALICE"
+    elif elice_win / elice_play < bob_win / bob_play:
+        result[t] = "#" + str(t+1) + " BOB"
+    
+for s in result:
+    print(s)
