11f3dc75a58f3f3de6f14ce29b3e28b432ff2089
K-Dongil <kdi1569@gmail.com>
2021-12-27 11:47:53 +0900

    간단한 계산 문제

--- baekjoon/Bronze/B3/1085.py
+++ baekjoon/Bronze/B3/1085.py
@@ -6,3 +6,17 @@ distance.append(now_y - 0)
 distance.append(right_up_y - now_y)
 
 print(min(distance))
+
+##############################################################
+now_x, now_y, right_up_x, right_up_y = map(int, input().split())
+min_distance = right_up_x + right_up_y
+if now_x < min_distance:
+    min_distance = now_x
+if right_up_x - now_x < min_distance:
+    min_distance = right_up_x - now_x
+if now_y < min_distance:
+    min_distance = now_y
+if right_up_y - now_y < min_distance:
+    min_distance = right_up_y -now_y
+
+print(min_distance)
