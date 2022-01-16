d1df1061d8d18f7b16c6b16633b79cee61a8ada8
K-dongil <kdi1569@gmail.com>
2022-01-17 04:59:48 +0900

    useEffect 실습

--- react/shop/src/Detail.js
+++ react/shop/src/Detail.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
@@ -12,8 +12,33 @@ let 제목 = styled.h4`
   color : ${ props => props.색상 }
 `;
 
+// class Detail2 extends React.Component {
+//   componentDidMount(){
+//     // Detail2 컴포넌트가 Mount(등장) 되었을 때 실행할 코드
+//   }
+
+//   componentWillUnmount(){
+//     // Detail2 컴포넌트가 UnMount(퇴장) 되었을 때 실행할 코드
+//   }
+// }
 
 function Detail(props){
+
+  
+
+  useEffect(()=>{
+    return function 컴포넌트퇴장() { console.log('unmount') }
+  });
+
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState();
+
+  useEffect(()=>{
+    // 2초 후에 alert 창을 안 보이게 하기
+    //let 타이머 = setTimeout(()=>{}, 2000)
+    let 타이머 = setTimeout(()=> {alert변경(false)}, 2000);
+    return ()=>{ clearTimeout(타이머) }
+  }, [alert]);
 
   let { id } = useParams();
   let history = useHistory();
@@ -27,9 +52,18 @@ function Detail(props){
       {/* <제목 색상={'red'}>Detail</제목> */}
       <제목 className="red">Detail</제목>
      </박스>
-     <div className="my-alert-yellow">
-       <p>재고가 얼마 남지 않았습니다</p>
-     </div>
+
+     { inputData }
+     <input onChange={(e)=>{inputData변경(e.target.value)}}></input>
+     
+     {
+       alert == true
+       ? (<div className="my-alert-yellow">
+           <p>재고가 얼마 남지 않았습니다</p>
+           </div>)
+       : null
+     }
+     
      <div className="row">
         <div className="col-md-6">
           <img src={firstUrl+middleUrl+endUrl} width="100%" />
