3c6109d5db2592fdf80b0a6846b90978847da961
K-dongil <kdi1569@gmail.com>
2022-01-24 21:44:20 +0900

    redux를 이용한 CRUD

--- react/redux/src/routes/Post/CommentList.js
+++ react/redux/src/routes/Post/CommentList.js
@@ -1,46 +1,53 @@
 import React, { useEffect, useState } from "react"
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import axios from 'axios';
 
 
 function CommentList() {
   const [comments, setcomments] = useState([]);
   const [inputComment, inputCommentChange] = useState();
   const [addCommentList, setaddCommentList] = useState();
+  const [dispatchComment, setDispatchComment] = useState();
+  
   const { num } = useParams();
   const nickname = "nickname";
 
-  let state = useSelector((state)=>state.commentReducer)
+  const store = useStore((state)=>state)
   let dispatch = useDispatch();
-  console.log(state)
 
-  useEffect( () => {
-    setcomments(state)
-  }, [])
   const changeComment = (e) => {
     inputCommentChange(e.target.value);
   }
 
-  const dispatchComment = {"postId" : parseInt(num), "nickname" : nickname, "comment":inputComment}
-
-  const addComment = e => {
-    setaddCommentList([...addCommentList], e.tatget.value);
+  const addComment = () => {
+    dispatch({ type : "add", inputComment : dispatchComment })
+    setcomments(store.getState().commentReducer)
   }
 
-  // useEffect(async ()=>{
-  //   try{
-  //     //api : http://localhost:3000/v1/post/{postId}/comment
-  //     const jsonData = await axios.get("http://localhost:3000/data/commentData.json")
-  //     setcomments(jsonData.data)
+  const deleteComment = (e) => {
+    dispatch({ type : "delete", i : e.target.attributes.commentid.value })
+    setcomments(store.getState().commentReducer)
+  }
+  
 
-  //   }
-  //   catch{
-  //     console.log("오류")
-  //   }
-  //   }, []
-  // )
-  console.log(comments)
+  useEffect(async ()=>{
+    try{
+      //api : http://localhost:3000/v1/post/{postId}/comment
+      const jsonData = await axios.get("http://localhost:3000/data/commentData.json")
+      dispatch({type:"dataLoading", jsonData : jsonData.data})
+      setcomments(store.getState().commentReducer)
+    }
+    catch{
+      console.log("오류")
+    }
+    }, []
+  )
+
+  useEffect( () => {
+    setDispatchComment({"postId" : parseInt(num), "nickname" : nickname, "comment":inputComment})
+  }, [inputComment])
+
   return(
     
     <div className="CommentList">
@@ -51,18 +58,15 @@ function CommentList() {
           value={ inputComment }
           onChange={ changeComment }
         />
-        <button type="button" onClick={ () => dispatch({ type : "add", inputComment : dispatchComment }) }>Add</button> <br/>
-        
-        <button type="submit" onClick={ addComment }>완료</button>
+        <button type="button" onClick={ addComment }>Add</button>
       </form>
 
       {
-        
         comments.map( (post, i) => {
           return(
             <div className="list" key={i}>
               <p> { post.comment } </p>
-              {/* <button type="button" onClick={ () => dispatch({ type : "change", comments: comments, i : i }) }></button> */}
+              <button class="deletebtn" type="button" commentId={i} onClick={ deleteComment }>삭제</button>
             </div>
           );
         })
--- react/redux/src/store.js
+++ react/redux/src/store.js
@@ -1,71 +1,30 @@
 import { combineReducers, createStore } from "redux"
-import axios from 'axios';
-import { useState } from "react";
-
-
-var a = null
-// const jsonAsync = async () =>{
-//   let jsonData = await axios.get("http://localhost:3000/data/commentData.json")
-//   return jsonData.data
-// }
-
-// const promise = getPromise()
-// const getData = () => {
-//   promise.then( jsonAsync => {
-//     console.log(jsonAsync);
-//   });
-// };
-// getData()
-
-// const jsonAsync = async () =>{
-//   const jsonData = await axios.get("http://localhost:3000/data/commentData.json").then((data)=>console.log(data))
-// }
-// jsonAsync()
-// console.log()
-
-// const jsonAsync = async () =>{
-//   const jsonData = await axios.get("http://localhost:3000/data/commentData.json").then((data)=>console.log(data))
-//   return jsonData
-// }
-// console.log(jsonAsync())
-
-const jsonAsync = async () => {
-  let jsonData = await axios.get("http://localhost:3000/data/commentData.json")
-  a = await jsonData.data
-  console.log(a)
-}
-
-jsonAsync()
-console.log('asdasdasd')
-console.log(a)
-
-
 
 const reducer = (state = [], action) => {
-  
   if (action.type === "recent"){
-    return [{text:'POPULAR'}]
-  } else {
     return [{text:'RECENT'}]
+  } else {
+    return [{text:'POPULAR'}]
   }
 }
 
 const commentReducer = (state = [], action) => {
-  console.log(state)
-  if (action.type === "add"){
-    console.log(state)
+  if (action.type === "dataLoading") {
+    state = action.jsonData
+    return state
+  }else if (action.type === "add"){
     const copyCommentList = [...state, action.inputComment]
-    return copyCommentList
-  }else if (action.type === "change"){
-    const copyCommentList = [...state]
-    // copyCommentList[action.i]
+    return copyCommentList // return된 copyCommentList가 state(기존reducer에 저장된 값)가 된다
+  }
+  else if (action.type === "delete"){
+    let copyCommentList = [...state]
+    copyCommentList.splice(action.i, 1)
     return copyCommentList
   }
   else{
     return state
   }
 }
-
 
 
 const store = createStore( combineReducers( {reducer, commentReducer} ))
