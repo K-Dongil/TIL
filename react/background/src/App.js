a5d03fa62bb5ee9bbe489246729441052a3400b4
K-dongil <kdi1569@gmail.com>
2022-02-06 19:04:26 +0900

    방울이 올라오는 배경

---
+++ react/background/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BubbleBackGround from './BubbleBackGround';
+
+function App() {
+  return (
+    <div className="App">
+      <BubbleBackGround />
+    </div>
+  );
+}
+
+export default App;
