8a0fbf5387de69f9d00aab513e6f04c1a060f2f3
K-dongil <kdi1569@gmail.com>
2022-03-07 23:11:01 +0900

    import

--- typeScript/quiz/2_address-book/src/index.ts
+++ typeScript/quiz/2_address-book/src/index.ts
@@ -1,18 +1,4 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneTypes {
-  'home', 'office', 'studio'
-}
+import { PhoneNumberDictionary, Contact, PhoneTypes } from './types'
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
