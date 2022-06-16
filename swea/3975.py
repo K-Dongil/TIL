5ea97dcaeaadbe1b3a21090e9aa871ef0289a7fa
K-Dongil <kdi1569@gmail.com>
2022-06-16 23:52:23 +0900

    TIL

--- swea/3975.py
+++ swea/3975.py
@@ -5,7 +5,9 @@ result = [0] * tc
 for t in range(0, tc):
     elice_win, elice_play, bob_win, bob_play = map(int, input().split())
     result[t] = "#" + str(t+1) + " DRAW"
-    if elice_win / elice_play > bob_win / bob_play:
+    if elice_win / elice_play == bob_win / bob_play:
+        continue
+    elif elice_win / elice_play > bob_win / bob_play:
         result[t] = "#" + str(t+1) + " ALICE"
     elif elice_win / elice_play < bob_win / bob_play:
         result[t] = "#" + str(t+1) + " BOB"
