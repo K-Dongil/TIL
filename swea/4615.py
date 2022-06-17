4290dc6ca28cdadb5642cf114fdd31e3a677cbff
K-Dongil <kdi1569@gmail.com>
2022-06-17 23:58:06 +0900

    TIL

--- swea/4615.py
+++ swea/4615.py
@@ -18,6 +18,50 @@ for t in range(1, tc+1):
     for i in range(chance):
         row, col, color = map(int, input().split())
         board[row][col] = color
+        for i in range(8):
+            sideR = row + dr[i]
+            sideC = col + dc[i]
+            if 1 <= sideR <= boardSize and 1 <= sideC <= boardSize and  board[sideR][sideC] == (3-color):
+                for j in range(2, boardSize):
+                    side2R = row + dr[i] * j
+                    side2C = col + dc[i] * j
+                    if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == 0:
+                        break
+                    if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == color:
+                        for k in range(1, j):
+                            a = row + dr[i] * k
+                            b = col + dc[i] * k
+                            board[a][b] = color
+                        break
+            
+                    
+    for lst in board:
+        black += lst.count(1)
+        white += lst.count(2)
+
+    print(f'#{t} {black} {white}')
+
+################################################################################
+dr = [-1, 1, 0, 0, -1, 1, -1, 1] # 상하좌우, 대각선(좌상, 좌하, 우상, 우하)
+dc = [0, 0, 1, -1, 1, 1, -1, -1]
+
+tc = int(input())
+
+for t in range(1, tc+1):
+    boardSize, chance = map(int, input().split())
+    board = [[0]*(boardSize+1) for _ in range(boardSize+1)]
+
+    board[boardSize//2][boardSize//2] = 2
+    board[boardSize//2 + 1][boardSize//2 + 1] = 2
+    board[boardSize//2 + 1][boardSize//2] = 1
+    board[boardSize//2][boardSize//2 + 1] = 1
+
+    black = 0
+    white = 0
+
+    for i in range(chance):
+        row, col, color = map(int, input().split())
+        board[row][col] = color
         if color == 1:
             for i in range(8):
                 sideR = row + dr[i]
@@ -29,7 +73,7 @@ for t in range(1, tc+1):
                         if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == 0:
                             break
                         if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == 1:
-                            for k in range(1, j-1):
+                            for k in range(1, j):
                                 a = row + dr[i] * k
                                 b = col + dc[i] * k
                                 board[a][b] = 1
@@ -46,7 +90,7 @@ for t in range(1, tc+1):
                         if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == 0:
                             break
                         if 1 <= side2R <= boardSize and 1 <= side2C <= boardSize and  board[side2R][side2C] == 2:
-                            for k in range(1, j-1):
+                            for k in range(1, j):
                                 a = row + dr[i] * k
                                 b = col + dc[i] * k
                                 board[a][b] = 2
--- swea/5177.py
+++ swea/5177.py
@@ -1,3 +1,11 @@
+def change(idx):
+    if idx == 1:
+        return None
+
+    if tree[idx] < tree[idx//2]:
+        tree[idx], tree[idx//2] = tree[idx//2], tree[idx]
+        change(idx//2)
+
 tc = int(input())
 
 for t in range(tc):
@@ -11,7 +19,7 @@ for t in range(tc):
     while idx <= node:
         tree[idx] = v_lst[idx-1]
         if tree[idx//2] > tree[idx]:
-            tree[idx//2], tree[idx]= tree[idx], tree[idx//2]
+            change(idx)
         idx += 1
 
     while node != 1:
@@ -19,3 +27,30 @@ for t in range(tc):
         sum += tree[node]
     
     print(f'#{t+1} {sum}')
+
+#===================================================
+# 재귀X
+# tc = int(input())
+
+# for t in range(tc):
+#     node = int(input())
+#     v_lst = list(map(int, input().split()))
+#     tree = [0]*(node+1)
+#     idx = 2
+#     sum = 0
+
+#     tree[1] = v_lst[0]
+#     while idx <= node:
+#         tree[idx] = v_lst[idx-1]
+#         if tree[idx//2] > tree[idx]:
+#             copy_idx = idx
+#             while tree[copy_idx//2] > tree[copy_idx]:
+#                 tree[copy_idx//2], tree[copy_idx]= tree[copy_idx], tree[copy_idx//2]
+#                 copy_idx = copy_idx//2
+#         idx += 1
+
+#     while node != 1:
+#         node = node // 2
+#         sum += tree[node]
+    
+#     print(f'#{t+1} {sum}')
