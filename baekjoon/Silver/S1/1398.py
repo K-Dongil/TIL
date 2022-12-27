bb94d8f46095edfd7c8f5c2d1cb9aa459da67198
K-Dongil <kdi1569@gmail.com>
2022-12-27 23:02:10 +0900

    인접행렬 bfs 최적경로 찾기

--- baekjoon/Silver/S1/1398.py
+++ baekjoon/Silver/S1/1398.py
@@ -1,20 +1,23 @@
 def bfs(v):
     visited = [False] * (userNum+1)
     queue = [(v, 0)]
+    visited[v] = True
     cnt = 0
 
     while queue:
-        nowUser = queue.pop()
-        visited[nowUser[0]] = True
-        for friend in graphs[nowUser[0]]:
-            if visited[friend] == False:
-                queue.append((friend, nowUser[1]+1))
+        nowUser = queue.pop(0)
+        for i in range(userNum+1):
+            if visited[i] == False and graphs[nowUser[0]][i]:
+                queue.append((i, nowUser[1]+1))
+                visited[i] = True
                 cnt += nowUser[1]+1
 
     return cnt
 
 userNum, relationNum = map(int, input().split())
 graphs = [[0]*(userNum+1) for _ in range(userNum+1)]
+minkevinBacon = 100 * 6
+winner = 0
 
 for _ in range(relationNum):
     x, y = map(int, input().split())
@@ -22,4 +25,9 @@ for _ in range(relationNum):
     graphs[y][x] = 1
 
 for user in range(1, userNum+1):
-    print(bfs(user))
+    kevinBacon = bfs(user)
+    if kevinBacon < minkevinBacon:
+        minkevinBacon = kevinBacon
+        winner = user
+
+print(winner)
