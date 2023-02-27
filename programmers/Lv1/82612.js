a598f0bdc6b415eaf9e1000d9b0c75d80a28f7d3
K-Dongil <kdi1569@gmail.com>
2023-02-27 22:33:31 +0900

    js for 코딩

---
+++ programmers/Lv1/82612.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let answer = 0;
+    
+    for (let i=1; i<=count; i++){
+        answer += (price * i);
+    }
+    
+    return money < answer ? answer - money : 0;
+}
