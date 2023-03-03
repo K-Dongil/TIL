d1d10f6cec1b4c608acefba8ea425c999a634759
K-Dongil <kdi1569@gmail.com>
2023-03-03 16:27:12 +0900

    인접한 땅의 오염도 조사 - bfs

---
+++ baekjoon/Silver/S1/1012_1.py
@@ -0,0 +1,42 @@
+def bfs(x, y):
+    queue = [(x,y)]
+    farm[y][x] = 0
+    while queue:
+        nowX, nowY = queue.pop(0)
+
+        for i in range(4):
+            newX = nowX + dx[i]
+            newY = nowY + dy[i]
+
+            if 0<=newY<height and 0<=newX<width and farm[newY][newX] == 1:
+                queue.append((newX,newY))
+                farm[newY][newX] = 0
+
+    for i in range(4):
+        newX = x+dx[i]
+        newY = y+dy[i]
+        if 0<=newY<height and 0<=newX<width and farm[newY][newX] == 1:
+            farm[newY][newX] = 0
+            dfs(newX, newY)
+
+dx = [0, 0 , 1, -1]
+dy = [-1, 1, 0, 0]
+
+T = int(input())
+
+for t in range(T):
+    width, height, vegetableNum = map(int, input().split())
+    farm = [[0]*width for _ in range(height)]
+    result = 0
+
+    for i in range(vegetableNum):
+        x, y = map(int, input().split())
+        farm[y][x] = 1
+    
+    for x in range(width):
+        for y in range(height):
+            if farm[y][x] == 1:
+                bfs(x, y)
+                result += 1
+    
+    print(result)
